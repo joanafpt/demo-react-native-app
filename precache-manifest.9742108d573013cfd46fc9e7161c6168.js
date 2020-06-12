cad985e3f31211ba8da5781b838a72f990c18722
Joana Teixeira <joanajteixeira@outlook.com>
2020-06-12 15:11:28 +0100

    Updates

---
+++ precache-manifest.9742108d573013cfd46fc9e7161c6168.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a790684fce430f45330c4894be6f7bbb",
+    "url": "/demo-react-native-app/index.html"
+  },
+  {
+    "revision": "2a1a838c3af34ef1f542",
+    "url": "/demo-react-native-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "bd4117810f0bf666ad32",
+    "url": "/demo-react-native-app/static/css/main.76515eb9.chunk.css"
+  },
+  {
+    "revision": "2a1a838c3af34ef1f542",
+    "url": "/demo-react-native-app/static/js/2.fcf6f63d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo-react-native-app/static/js/2.fcf6f63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4117810f0bf666ad32",
+    "url": "/demo-react-native-app/static/js/main.b5d31e7a.chunk.js"
+  },
+  {
+    "revision": "145e0cd4cc60e90ecf3d",
+    "url": "/demo-react-native-app/static/js/runtime-main.848d9246.js"
+  },
+  {
+    "revision": "5ae0493a06dccba3d350ea859494fc50",
+    "url": "/demo-react-native-app/static/media/chamada_api_ios.5ae0493a.png"
+  },
+  {
+    "revision": "99e578aae0a8e7800007e093e6499aba",
+    "url": "/demo-react-native-app/static/media/chamda_api_android.99e578aa.png"
+  },
+  {
+    "revision": "894012b8049df6ac786a3cbab9927b96",
+    "url": "/demo-react-native-app/static/media/dropdown_ios.894012b8.png"
+  },
+  {
+    "revision": "98dddba433e4a829db89ec20d4002cc5",
+    "url": "/demo-react-native-app/static/media/ecra_dropdown_android.98dddba4.png"
+  },
+  {
+    "revision": "032f888c61a83626c57a698192fa111b",
+    "url": "/demo-react-native-app/static/media/ecra_pf_aguarde_android.032f888c.png"
+  },
+  {
+    "revision": "ed42e9b5eb5d425938c575f7220a8701",
+    "url": "/demo-react-native-app/static/media/initAndroid.ed42e9b5.jpg"
+  },
+  {
+    "revision": "9076eda812877f340304b3c71f63dcb1",
+    "url": "/demo-react-native-app/static/media/menu_inicial_ios.9076eda8.png"
+  },
+  {
+    "revision": "dbadbd25cbdf104d9abcf5c5d10f1559",
+    "url": "/demo-react-native-app/static/media/pf_aguarde_ios.dbadbd25.png"
+  },
+  {
+    "revision": "bfbd974c56f37f054020670c3b7ff784",
+    "url": "/demo-react-native-app/static/media/resp_servidor_ios.bfbd974c.png"
+  },
+  {
+    "revision": "168736993996fadf0bd79c8df9e6f6e6",
+    "url": "/demo-react-native-app/static/media/resposta_api_android.16873699.png"
+  }
+]);
